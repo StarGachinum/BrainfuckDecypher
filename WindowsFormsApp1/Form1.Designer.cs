fa47b2bf45be7632336de760a7243c1cbeda3e78
StarGachinum <sharko.yura2601@gmail.com>
2024-01-19 21:50:00 +0200

    Start commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.brainfuckTextField = new System.Windows.Forms.TextBox();
+            this.normalTextField = new System.Windows.Forms.TextBox();
+            this.decypherButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // brainfuckTextField
+            // 
+            this.brainfuckTextField.Location = new System.Drawing.Point(21, 121);
+            this.brainfuckTextField.Name = "brainfuckTextField";
+            this.brainfuckTextField.Size = new System.Drawing.Size(747, 20);
+            this.brainfuckTextField.TabIndex = 0;
+            // 
+            // normalTextField
+            // 
+            this.normalTextField.Location = new System.Drawing.Point(21, 40);
+            this.normalTextField.Name = "normalTextField";
+            this.normalTextField.Size = new System.Drawing.Size(747, 20);
+            this.normalTextField.TabIndex = 1;
+            // 
+            // decypherButton
+            // 
+            this.decypherButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decypherButton.Location = new System.Drawing.Point(306, 74);
+            this.decypherButton.Name = "decypherButton";
+            this.decypherButton.Size = new System.Drawing.Size(136, 34);
+            this.decypherButton.TabIndex = 2;
+            this.decypherButton.Text = "Decypher";
+            this.decypherButton.UseVisualStyleBackColor = true;
+            this.decypherButton.Click += new System.EventHandler(this.decypherButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Normal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Brainfuck";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 165);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.decypherButton);
+            this.Controls.Add(this.normalTextField);
+            this.Controls.Add(this.brainfuckTextField);
+            this.Name = "Form1";
+            this.Text = "BFDecypher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox brainfuckTextField;
+        private System.Windows.Forms.TextBox normalTextField;
+        private System.Windows.Forms.Button decypherButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private int fieldWithTextIndex;//BF = 0 Normal = 1
+
+        public Form1()
+        {
+            InitializeComponent(); 
+        }
+
+        public void FindFieldWithText()
+        {
+            CheckIfDebil();
+            if (brainfuckTextField.Text != "")
+            {
+                fieldWithTextIndex = 0;
+            }
+            else
+            {
+                fieldWithTextIndex = 1;
+            }
+        }
+        public void CheckIfDebil()
+        {
+            if (brainfuckTextField.Text != "" && normalTextField.Text != "")
+            {
+                brainfuckTextField.Text = "";
+                normalTextField.Text = "";
+                MessageBox.Show("Are you OK?");
+            }
+        }
+
+        private void decypherButton_Click(object sender, EventArgs e)
+        {
+            FindFieldWithText();
+            ChooseDecypher();
+        }
+
+        private void ChooseDecypher()
+        {
+            if (fieldWithTextIndex == 0)
+            {
+                normalTextField.Text = ConvertToNormal(brainfuckTextField.Text, normalTextField.Text);
+                brainfuckTextField.Text = "";
+            }
+            else
+            {
+                brainfuckTextField.Text = ConvertToBrainfuck(normalTextField.Text, brainfuckTextField.Text);
+                normalTextField.Text = "";
+            }
+        }
+
+        private string ConvertToBrainfuck(string from, string to)
+        {
+            byte[] ASCIIcodes = Encoding.ASCII.GetBytes(from);
+            int length = ASCIIcodes.Length;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < ASCIIcodes[i]; j++)
+                {
+                    to += "+";
+                }
+                to += ".>";
+            }
+            return to;
+        }
+
+        private string ConvertToNormal(string from, string to)
+        {
+            string[] bfCells = from.Split('>');
+            int length = bfCells.Length;
+            byte[] acsiiCodes = new byte[length];
+            for (int i = 0; i < length; i++)
+            {
+                byte acsiiCode = 0;
+                foreach (char cha in bfCells[i])
+                {
+                    if (cha == '+')
+                        acsiiCode++;
+                }
+                acsiiCodes[i] = acsiiCode;
+            }
+
+            to += Encoding.ASCII.GetString(acsiiCodes);
+            return to;
+
+        }
+    }
+}
